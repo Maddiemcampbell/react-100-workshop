31e78567d39243493d3321c597fb8c6a663d3403
Maddie <maddiemcampbell@gmail.com>
2019-09-17 22:14:02 -0700

    functionality mark boxes with x when clicked

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Square extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      value: null,
+    };
+  }
+
   render() {
     return (
-      <button className="square" onClick={() => alert('click')}>
-        {this.props.value}
+      <button className="square" onClick={() => this.setState({value: 'X'})}>
+        {this.state.value}
       </button>
     );
   }
